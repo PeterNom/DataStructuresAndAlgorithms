1ead0643b3d7f56bec1a92311fc5bdc9361bc854
Panagiotis Nomikos <peternomik@gmail.com>
2023-11-15 21:18:17 +0200

    Add project files.

---
+++ DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructuresAndAlgorithms;
+
+Console.WriteLine("Hello, World!");
+int[] ints = { 5, 2, 4, 6, 1, 3};
+
+SortingArlgorims mySort = new SortingArlgorims(ints);
+
+int[] ints1 = mySort.InsertionSort_2();
+
+foreach (int i in ints1)
+    Console.WriteLine(i);
---
+++ DataStructuresAndAlgorithms/SortingArlgorims.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    internal class SortingArlgorims
+    {
+        private int[] my_array;
+        
+        public SortingArlgorims(int[] arrayToBeSorted) 
+        {
+            my_array = arrayToBeSorted;
+        }
+        //My implementation
+        public int[] InsertionSort_1()
+        {
+            for(int x = 1; x < my_array.Length; x++)
+            {
+                int y = x;
+                while (y>0 && (my_array[y] < my_array[y-1]))
+                {
+                    int temp = my_array[y - 1];
+                    my_array[y-1] = my_array[y];
+                    my_array[y] = temp;
+                    y--;
+                }
+            }
+            return my_array;
+        }
+
+        //Book implementation
+        public int[] InsertionSort_2()
+        {
+            for (int x = 1; x < my_array.Length; x++)
+            {
+                int key = my_array[x];
+                int y = x - 1;
+
+                while (y >= 0 && my_array[y]>key)
+                {
+                    my_array[y+1] = my_array[y];
+                    y--;
+                }
+
+                my_array[y + 1] = key;
+            }
+            return my_array;
+        }
+
+        public int[] Mergesort()
+        {
+            return my_array;
+        }
+        public int[] BubbleSort()
+        {
+            return my_array;
+        }
+
+        public int[] Heapsort()
+        {
+            return my_array;
+        }
+
+        public int[] Quicksort()
+        {
+            return my_array;
+        }
+
+        public int[] Heapsort()
+        {
+            return my_array;
+        }
+    }
+}
