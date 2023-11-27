17fbf818f3cf10f55b57158cceed73f0c74e688e
Panagiotis Nomikos <peternomik@gmail.com>
2023-11-27 14:43:21 +0200

    Added some foundation for Graphs and Heap implementation.

---
+++ DataStructuresAndAlgorithms/Graph.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    class Graph
+    {
+        private int TotalNodes;
+
+        private LinkedList<int>[] adj;
+
+        public Graph(int numOfNodes)
+        {
+            TotalNodes = numOfNodes;
+            adj = new LinkedList<int>[numOfNodes];
+
+            for (int i = 0; i < adj.Length; i++)
+                adj[i] = new LinkedList<int>();
+        }
+
+        void AddEdge(int nodeStart, int nodeEnd)
+        { 
+            adj[nodeStart].AddLast(nodeEnd); 
+        }
+
+        public void GraphInitialize(Graph graph)
+        {
+            this.AddEdge(0, 1);
+            this.AddEdge(0, 2);
+            this.AddEdge(1, 2);
+            this.AddEdge(2, 0);
+            this.AddEdge(2, 3);
+            this.AddEdge(3, 3);
+        }
+
+        public void GraphInfo() 
+        {
+            foreach(var edge in this.adj)
+            {
+                foreach(var po in edge)
+                    Console.WriteLine(po);
+                Console.WriteLine("New node.");
+            }
+        }
+
+        public void BFS(int node)
+        {
+
+        }
+
+        public void DFS(int node)
+        {
+
+        }
+    }
+}
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using DataStructuresAndAlgorithms;
+﻿using DataStructuresAndAlgorithms;
 
 Console.WriteLine("Hello, World!");
 int[] ints   = { 12, 11, 13, 5, 6, 7 };
@@ -7,20 +6,29 @@ int[] subMax = { 13,-3,-25,20, -3, -16, -23, 18, 20, -7, 12, -5,-22,15,-4,7 };
 
 SortingAlgorithms mySort = new SortingAlgorithms(ints);
 MinMaxAlgorithms myMax = new MinMaxAlgorithms(subMax);
+Graph graph = new Graph(4);
 
+// Insertion Sort
 int[] ints1 = mySort.InsertionSortV2();
 
 foreach (int i in ints1)
     Console.WriteLine(i);
 
+// Bubble Sort
 int[] ints2 = mySort.BubbleSort();
 
 foreach (int i in ints2)
     Console.WriteLine(i);
 
+// Merge Sort
 mySort.Mergesort(0, mySort.GetArray().Length-1);
 
 foreach (int i in mySort.GetArray())
     Console.WriteLine(i);
 
+// Maximum Sub Array
 Console.WriteLine(myMax.FindMaximumSubarray(0, myMax.GetArray().Length-1));
+
+//BFS
+graph.GraphInitialize(graph);
+
